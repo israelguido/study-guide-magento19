52a16fc52d16fd9200279f6b53d032535827d08a
Israel Guido <israel@webjump.com.br>
2016-09-28 11:37:01 -0300

    Adicionando conteudo a um controller no magento

--- addContentInController.php
+++ addContentInController.php
@@ -4,8 +4,18 @@
 ## usei num controller mas pode ser usado em qualquer lugar 
 
 $this->loadLayout();
-    $block = $this->getLayout()->createBlock('core/template');
-        $block->setTemplate('helpmebuy/finalpage.phtml');
+	$block = $this->getLayout()->createBlock('core/template');
+	    $block->setTemplate('helpmebuy/finalpage.phtml');
+
+	$this->getLayout()->getBlock('content')->append($block);
+
+	$this->renderLayout();
+
+## OR 
+
+$this->loadLayout();
+    $block = $this->getLayout()->createBlock('customer/form_login');
+    $block->setTemplate('persistent/customer/form/login.phtml');
 
     $this->getLayout()->getBlock('content')->append($block);
 
